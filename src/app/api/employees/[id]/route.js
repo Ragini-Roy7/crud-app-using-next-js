5a382a0dabe9443db5d59489f57ce93d5f56214b
Ragini-Roy7 <royragini967@gmail.com>
2025-10-05 21:01:44 +0530

    Fixed API imports, resolved employees data path , and updated CRUD routes

--- src/app/api/employees/[id]/route.js
+++ src/app/api/employees/[id]/route.js
@@ -1,26 +1,32 @@
-import {employees} from "../data"
+import { employees } from "../data"; 
 import { NextResponse } from "next/server";
-
-export async function GET(req, { params }) {
-  const { id } = params; 
-  const emp = employees.find((e) => e.id === Number(id));
-  return NextResponse.json(emp || {});
-}
 
 export async function PUT(req, { params }) {
   const { id } = params;
   const body = await req.json();
-  employees = employees.map((e) =>
-    e.id === Number(id) ? { ...e, ...body } : e
-  );
-  return NextResponse.json({ success: true });
+
+  const index = employees.findIndex((e) => e.id === Number(id));
+  if (index === -1)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+
+  Object.assign(employees[index], body);
+
+  return NextResponse.json({
+    success: true,
+    employee: employees[index],
+  });
 }
 
 export async function DELETE(req, { params }) {
   const { id } = params;
+
   const index = employees.findIndex((e) => e.id === Number(id));
-  if (index === -1) return NextResponse.json({ error: "Not found" }, { status: 404 });
+  if (index === -1)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+
   employees.splice(index, 1);
   return NextResponse.json({ success: true });
 }
+
+
 
--- src/app/api/employees/route.js
+++ src/app/api/employees/route.js
@@ -1,13 +1,5 @@
-
+import {employees } from "./data";
 import { NextResponse } from "next/server";
-
-let employees = [
-  { id: 1, name: "Ravi Kumar", role: "Developer", email: "ravikr@example.com" },
-  { id: 2, name: "Ayesha Singh", role: "UI/UX Designer", email: "ayeshasingh@example.com" },
-  { id: 3, name: "Ragini Roy", role: "Frontend Developer", email: "royragini123@example.com" },
-  { id: 4, name: "Siddhartha Kumar", role: "Fullstack Developer", email: "sid12@example.com" },
-  { id: 5, name: "Rimjhim Sinha", role: "UI/UX Designer", email: "sinharimjhim@example.com" },
-];
 
 export async function GET() {
   return NextResponse.json(employees);
